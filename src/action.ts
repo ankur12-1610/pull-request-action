97bfbadb5135441193bbf3bc63f400b5773fe719
ankur12-1610 <anknerd.12@gmail.com>
2021-12-28 23:22:32 +0530

    Set up workflow and managed dependencies

---
+++ src/action.ts
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const core = require('@actions/core')
+const github = require('@actions/github')
+const { Octokit } = require("@octokit/rest");
+
+async function run() {
+  try {
+    const { context } = require('@actions/github')
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
+    const comment = core.getInput('content')
+    const reaction = core.getInput('reaction')
+    const tag_creator = core.getInput('tag_creator')
+
+    if ( typeof GITHUB_TOKEN !== 'string' ) {
+      throw new Error('Invalid GITHUB_TOKEN: did you forget to set it in your action config?');
+    }
+ 
+    const octokit = new Octokit({
+        auth: GITHUB_TOKEN
+    });
+    const { pull_request } = context.payload
+    const payload = context.payload.pull_request
+    const author = payload.user.login
+    
+    const tag_text = (tag_creator ? `@` + author + ` ` : null) 
+
+    await octokit.rest.issues.createComment({
+      ...context.repo,
+      issue_number: pull_request.number,
+      body: tag_text + comment,
+      id: payload.number.toString()
+    })
+
+    await octokit.rest.issues.addAssignees({
+      ...context.repo,
+      issue_number: pull_request.number,
+      assignees: author
+    })
+
+    await octokit.rest.reactions.createForIssue({
+      ...context.repo,
+      repo: context.repo.repo,
+      issue_number: pull_request.number,
+      content: reaction,
+      owner: context.repo.owner
+    })
+  } catch (e) {
+    core.error(e)
+    core.setFailed((e as Error).message)
+  }
+}
+
+
+
+run()
