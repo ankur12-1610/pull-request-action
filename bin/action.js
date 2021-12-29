32ce4567b808f8e2b2bd99ea77d76a3c51d3a17f
ankur12-1610 <anknerd.12@gmail.com>
2021-12-29 18:42:28 +0530

    :rocket: added giphy support

--- bin/action.js
+++ bin/action.js
@@ -8,42 +8,37 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fetch = require('node-fetch');
+const axios_1 = __importDefault(require("axios"));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const { context } = require('@actions/github');
-            const TENOR_TOKEN = core.getInput('TENOR_TOKEN') || process.env.TENOR_TOKEN;
+            const GIPHY_TOKEN = core.getInput('GIPHY_TOKEN');
             const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
             const COMMENT_TEXT = core.getInput('COMMENT_TEXT');
-            const searchTerm = core.getInput('searchTerm') || 'thank you';
             const PR_REACTION = core.getInput('PR_REACTION');
             const TAG_AUTHOR = core.getInput('TAG_AUTHOR');
             const ASSIGN_TO_AUTHOR = core.getInput('ASSIGN_TO_AUTHOR');
-            if (typeof TENOR_TOKEN !== 'string') {
-                throw new Error('Invalid TENOR_TOKEN: did you forget to set it in your action config?');
-            }
             if (typeof GITHUB_TOKEN !== 'string') {
                 throw new Error('Invalid GITHUB_TOKEN: did you forget to set it in your action config?');
             }
-            //gif
-            const randomPos = Math.round(Math.random() * 1000);
-            const url = `https://api.tenor.com/v1/search?q=${encodeURIComponent(searchTerm)}&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high`;
-            console.log(`${url}`);
-            const response = yield fetch(`${url}&key=${TENOR_TOKEN}`);
-            const { results } = yield response.json();
-            const gifUrl = results[0].media[0].tinygif.url;
-            console.log(`${gifUrl}`);
+            const response = yield axios_1.default.get(`https://api.giphy.com/v1/gifs/random?api_key=${GIPHY_TOKEN}&tag=thanksalot`);
+            const gifUrl = response.data.data.images.fixed_height_small.url;
+            const gif = `\n\n![thanks](${gifUrl})`;
             const octokit = github.getOctokit(GITHUB_TOKEN);
             const { pull_request } = context.payload;
             const payload = context.payload.pull_request;
             const author = payload.user.login;
             const tag_text = (TAG_AUTHOR ? `@` + author + ` ` : null);
             const assignee = (ASSIGN_TO_AUTHOR ? author : null);
-            yield octokit.rest.issues.createComment(Object.assign(Object.assign({}, context.repo), { issue_number: pull_request.number, body: tag_text + COMMENT_TEXT + `\n\n<img src="${gifUrl}" alt="${searchTerm}"/>`, id: payload.number.toString() }));
+            yield octokit.rest.issues.createComment(Object.assign(Object.assign({}, context.repo), { issue_number: pull_request.number, body: tag_text + COMMENT_TEXT + gif, id: payload.number.toString() }));
             yield octokit.rest.issues.addAssignees(Object.assign(Object.assign({}, context.repo), { issue_number: pull_request.number, assignees: assignee }));
             yield octokit.rest.reactions.createForIssue(Object.assign(Object.assign({}, context.repo), { repo: context.repo.repo, issue_number: pull_request.number, content: PR_REACTION, owner: context.repo.owner }));
         }
--- src/action.ts
+++ src/action.ts
@@ -1,36 +1,31 @@
 require('dotenv').config()
 const core = require('@actions/core')
 const github = require('@actions/github')
-const fetch = require('node-fetch')
+import axios, { AxiosInstance, AxiosResponse } from 'axios'
+
+declare module 'axios' {
+  interface AxiosResponse<T = any> extends Promise<T> {}
+}
 
 async function run() {
     try {
     const { context } = require('@actions/github')
-    const TENOR_TOKEN = core.getInput('TENOR_TOKEN') || process.env.TENOR_TOKEN
+    const GIPHY_TOKEN = core.getInput('GIPHY_TOKEN')
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
     const COMMENT_TEXT = core.getInput('COMMENT_TEXT')
-    const searchTerm = core.getInput('searchTerm') || 'thank you';
     const PR_REACTION = core.getInput('PR_REACTION')
     const TAG_AUTHOR = core.getInput('TAG_AUTHOR')
     const ASSIGN_TO_AUTHOR = core.getInput('ASSIGN_TO_AUTHOR')
     
-    if ( typeof TENOR_TOKEN !== 'string' ) {
-      throw new Error('Invalid TENOR_TOKEN: did you forget to set it in your action config?');
-    }
   
     if ( typeof GITHUB_TOKEN !== 'string' ) {
       throw new Error('Invalid GITHUB_TOKEN: did you forget to set it in your action config?');
     }
+    
+    const response: AxiosResponse =  await axios.get(`https://api.giphy.com/v1/gifs/random?api_key=${GIPHY_TOKEN}&tag=thanksalot`)
+    const gifUrl: string = response.data.data.images.fixed_height_small.url
 
-    //gif
-    const randomPos = Math.round(Math.random() * 1000)
-    const url = `https://api.tenor.com/v1/search?q=${encodeURIComponent(searchTerm)}&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high`
-    console.log(`${url}`)
-    const response = await fetch(`${url}&key=${TENOR_TOKEN}`)
-    const { results } = await response.json()
-    const gifUrl = results[0].media[0].tinygif.url
-    console.log(`${gifUrl}`)
-
+    const gif = `\n\n![thanks](${gifUrl})`
     const octokit = github.getOctokit(GITHUB_TOKEN) 
     const { pull_request } = context.payload
     const payload = context.payload.pull_request
@@ -41,7 +36,7 @@ async function run() {
     await octokit.rest.issues.createComment({
       ...context.repo,
       issue_number: pull_request.number,
-      body: tag_text + COMMENT_TEXT + `\n\n<img src="${gifUrl}" alt="${searchTerm}"/>`,
+      body: tag_text + COMMENT_TEXT + gif,
       id: payload.number.toString()
     })
 
@@ -63,7 +58,5 @@ async function run() {
     core.setFailed((e as Error).message)
   }
 }
-
-
 
 run()
