7861961b4d7cfe4d25a39a5a1afecaf70ff02b88
ankur12-1610 <anknerd.12@gmail.com>
2021-12-29 14:07:44 +0530

    Stashed changes

--- src/action.ts
+++ src/action.ts
@@ -1,12 +1,12 @@
 require('dotenv').config()
 const core = require('@actions/core')
 const github = require('@actions/github')
-const fetch = require('node-fetch')
+import axios from 'axios'
 
 async function run() {
     try {
     const { context } = require('@actions/github')
-    const TENOR_TOKEN = core.getInput('TENOR_TOKEN') || process.env.TENOR_TOKEN
+    const GIPHY_TOKEN = core.getInput('GIPHY_TOKEN') || process.env.TENOR_TOKEN 
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
     const COMMENT_TEXT = core.getInput('COMMENT_TEXT')
     const searchTerm = core.getInput('searchTerm') || 'thank you';
@@ -14,7 +14,7 @@ async function run() {
     const TAG_AUTHOR = core.getInput('TAG_AUTHOR')
     const ASSIGN_TO_AUTHOR = core.getInput('ASSIGN_TO_AUTHOR')
     
-    if ( typeof TENOR_TOKEN !== 'string' ) {
+    if ( typeof GIPHY_TOKEN !== 'string' ) {
       throw new Error('Invalid TENOR_TOKEN: did you forget to set it in your action config?');
     }
   
@@ -23,13 +23,9 @@ async function run() {
     }
 
     //gif
-    const randomPos = Math.round(Math.random() * 1000)
-    const url = `https://api.tenor.com/v1/search?q=${encodeURIComponent(searchTerm)}&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high`
-    console.log(`${url}`)
-    const response = await fetch(`${url}&key=${TENOR_TOKEN}`)
-    const { results } = await response.json()
-    const gifUrl = results[0].media[0].tinygif.url
-    console.log(`${gifUrl}`)
+    const response = await axios.get(`https://api.giphy.com/v1/gifs/random?api_key=${GIPHY_TOKEN}&tag=thanks&rating=g`)
+    const gif = `![welcome_gif](${response.data.images.original.url})`;
+    console.log(gif)
 
     const octokit = github.getOctokit(GITHUB_TOKEN) 
     const { pull_request } = context.payload
@@ -41,7 +37,7 @@ async function run() {
     await octokit.rest.issues.createComment({
       ...context.repo,
       issue_number: pull_request.number,
-      body: tag_text + COMMENT_TEXT + `\n\n<img src="${gifUrl}" alt="${searchTerm}"/>`,
+      body: tag_text + COMMENT_TEXT + gif,
       id: payload.number.toString()
     })
 
