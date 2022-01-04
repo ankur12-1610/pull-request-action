ca574a11a3f52e8ee62c29a8be02dea17bd6b2da
ankur12-1610 <anknerd.12@gmail.com>
2022-01-04 12:53:36 +0530

    Added first timers message

--- src/action.ts
+++ src/action.ts
@@ -19,6 +19,7 @@ async function run() {
     const TAG_AUTHOR = core.getInput('TAG_AUTHOR')
     const ASSIGN_TO_AUTHOR = core.getInput('ASSIGN_TO_AUTHOR')
     const GIPHY_TOPIC = camelCase(core.getInput('GIPHY_TOPIC'))
+    const FIRST_TIMERS_MESSAGE = core.getInput('FIRST_TIMERS_MESSAGE')
     
   
     if ( typeof GITHUB_TOKEN !== 'string' ) {
@@ -30,7 +31,6 @@ async function run() {
     const gifUrl: string = response.data.data.images.fixed_height_small.url
 
 
-    const sender = context.payload.sender.login
     const gif = `\n\n![thanks](${gifUrl})`
     const octokit = github.getOctokit(GITHUB_TOKEN) 
     const { pull_request } = context.payload
@@ -39,6 +39,27 @@ async function run() {
     const tag_text = (TAG_AUTHOR ? `@` + author + ` ` : null) 
     const assignee = (ASSIGN_TO_AUTHOR ? author : null)
     
+    //get no of prs created by author
+    const prs_by_author = await octokit.pulls.list({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+    })
+    const prs_by_author_count = prs_by_author.data.length
+    
+    if(prs_by_author_count > 1) {
+      core.setOutput('PR_REACTION', PR_REACTION)
+      core.setOutput('COMMENT_TEXT', COMMENT_TEXT + gif)
+      core.setOutput('TAG_AUTHOR', tag_text)
+      core.setOutput('ASSIGN_TO_AUTHOR', assignee)
+      core.setOutput('FIRST_TIMERS_MESSAGE', null)
+    } else {
+      core.setOutput('PR_REACTION', null)
+      core.setOutput('COMMENT_TEXT', null)
+      core.setOutput('TAG_AUTHOR', null)
+      core.setOutput('ASSIGN_TO_AUTHOR', null)
+      core.setOutput('FIRST_TIMERS_MESSAGE', FIRST_TIMERS_MESSAGE)
+    }
+
     //comment on PR
     await octokit.rest.issues.createComment({
       ...context.repo,
