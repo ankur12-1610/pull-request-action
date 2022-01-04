2c4f3b99d5c1d1142315fae6c27c0af7cb7bb02a
ankur12-1610 <anknerd.12@gmail.com>
2022-01-04 12:53:36 +0530

    Added first timers message

---
+++ bin/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const activity_1 = require("./activity");
+(0, activity_1.run)();
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import { run } from './activity'
+run()
